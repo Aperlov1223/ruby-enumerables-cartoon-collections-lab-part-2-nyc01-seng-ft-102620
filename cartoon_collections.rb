fd8af9b6095328b2998d4433e501b8df6e953394
Alice Perlov <aperlov23@gmail.com>
2020-10-09 17:58:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,7 @@ end # Use an Enumerable to check if any string in the passed in array is greater
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |calls|
+  valid_calls.find do |calls|
   end# Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
 end
