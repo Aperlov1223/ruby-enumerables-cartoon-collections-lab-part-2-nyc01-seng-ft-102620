f8206cae07d738f4d50c6b9522c6dce98fb61eca
Alice Perlov <aperlov23@gmail.com>
2020-10-09 17:39:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,6 +10,7 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |calls|
       "#{calls.capitalize}!"
    end
+ end
 
 def long_planeteer_calls(planeteer_calls)
 array.any? do |word|
