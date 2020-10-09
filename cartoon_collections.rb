50cf43dcf433a96d5c9e2aedb5cced11b1107f09
Alice Perlov <aperlov23@gmail.com>
2020-10-09 17:19:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 def summon_captain_planet(planeteer_calls)
   new_calls = []
    planeteer_calls.collect do |calls|
-     new_calls << "#{new_calls.upcase}!"
+     planeteer_calls << "#{new_calls.upcase}!"
    end
    summon_captain_planet(planeteer_calls)  # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
